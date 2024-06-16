5e29928a4cba0486dcd1fa91141ce666d11f2c68
alejandro <alejandro_velasco33@hotmail.com>
2024-06-16 21:40:51 +0200

    agregadas corrutinas

--- composeApp/src/androidMain/kotlin/App.kt
+++ composeApp/src/androidMain/kotlin/App.kt
@@ -1,34 +1,35 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
-import tocounttotell.composeapp.generated.resources.Res
-import tocounttotell.composeapp.generated.resources.compose_multiplatform
+
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+        val greeting = remember { Greeting().greet() }
+
+        Column(
+            modifier = Modifier.padding(all = 20.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
+            greeting.forEach { greeting ->
+                Text(greeting)
+                Divider()
             }
         }
     }
