b55bdfbb82156fdadc97e7faa8a07e016fd084c5
alejandro <alejandro_velasco33@hotmail.com>
2024-09-09 18:36:29 +0200

    agregando dependencias de sqlite

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.sqldelight)
 }
 
 kotlin {
@@ -24,11 +25,12 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     sourceSets {
         commonMain.dependencies {
-            // put your Multiplatform dependencies here
+            implementation(libs.sqldelight.runtime)
         }
+
     }
 }
 
@@ -41,5 +43,12 @@ android {
     }
     defaultConfig {
         minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
+
+sqldelight {
+    database("Database") {
+        packageName = "com.example"
+        // Puedes agregar más configuraciones aquí, si es necesario
     }
 }
